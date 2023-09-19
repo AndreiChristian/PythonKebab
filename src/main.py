72891632945db72eaa294cbe945375a00867ebc2
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-09-19 21:00:32 +0300

    day 1

---
+++ src/main.py
@@ -0,0 +1,52 @@
+import produs
+from suc import alege_suc
+
+program_activ = True  # Definim o variabila "program_activ" care porneste ca fiind True
+lista_produse = []
+
+
+while program_activ:  # Cat timp program_activ este true
+
+    print("Bine ati venit la Python Kebab!")  # Printam un mesaj de bun venit
+
+    print("1.Kebab\n2.Suc\n3.Cartofi\n4.Meniu\n5.Arata cosul\n6.Pretul total")
+
+    # Intrebam utilizatorul ce doreste sa
+    comanda = input("Ce doriti sa comandati? ")
+    # comande si stocam comanda utilizatorului intr-o variabila
+    if comanda == 'q':  # Daca comanda este 'q' ne luam la revedere si oprim programul,
+        # deoarece program_activ devine False
+        print("La revedere")
+        program_activ = False
+
+    elif comanda == "1":
+        print("Optiunea selectata este nr 1")
+
+    elif comanda == "2":
+        alege_suc(lista_produse)
+
+    elif comanda == "3":
+        pass
+
+    elif comanda == "4":
+        pass
+
+    elif comanda == "5":
+
+        for produs in lista_produse:  # Pentru fiecare produs din lista de produse,
+            # produsul se va prezenta, folosind metoda de prezentare din produs.py
+            produs.prezentare()
+
+    elif comanda == "6":
+        suma = 0
+        for produs in lista_produse:
+            suma += produs.pret
+
+        print(f"Pretul total este {suma}")
+
+    else:
+
+        # Altfel, printam comanda utilizatorului
+        print(f"Optiunea selectata este:{comanda}")
+
+    # si cerem urmatoarea comanda
---
+++ src/produs.py
@@ -0,0 +1,20 @@
+class Produs:
+
+    def __init__(self, denumire, pret):
+        self.denumire = denumire
+        self.pret = pret
+
+    def prezentare(self):
+        print(f"Denumire: {self.denumire}, Pret = {self.pret}")
+
+
+# suc1 = Cola()
+# suc1.prezentare()
+
+
+# suc2 = Fanta()
+# suc2.prezentare()
+
+
+# suc3 = Sprite()
+# suc3.prezentare()
